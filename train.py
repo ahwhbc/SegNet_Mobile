8087b67f80e25391c86e6b54a3abe4adfc96bd6a
Chern B <34539822+ahwhbc@users.noreply.github.com>
2020-04-08 15:06:45 +0800

    Update train.py

--- train.py
+++ train.py
@@ -113,7 +113,7 @@ if __name__ == "__main__":
             steps_per_epoch=max(1, num_train//batch_size),
             validation_data=generate_arrays_from_file(lines[num_train:], batch_size),
             validation_steps=max(1, num_val//batch_size),
-            epochs=50,
+            epochs=2,
             initial_epoch=0,
             callbacks=[checkpoint_period, reduce_lr])
 
